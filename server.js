64a6aab3d25b7786e79c2bd8f2bc42ead6a279f4
harsh0506 <joshiharsh0506@gmail.com>
2023-05-27 10:42:28 +0530

    added html

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const port = process.env.PORT || 4500; // Change this to your desired port
 
 app.use(express.json());
 
+app.use(express.static('public'));
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
